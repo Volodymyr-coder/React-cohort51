b9a6b2f42effd6566770ff5378d2145a5eb795dd
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2025-04-18 17:12:20 +0200

    home page with render all product

--- week2/project/ecommerce/components/Home.jsx
+++ week2/project/ecommerce/components/Home.jsx
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import css from './Home.module.css';
 const Home = () => {
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const fetchAllData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchAllData();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className={css.title}>Welcome in our shop!</h1>
+      <ul className={css.gridContainer}>
+        {products.map((item) => {
+          return (
+            <li className={css.item} key={item.id}>
+              <img className={css.img} src={item.image} />
+              <p className={css.title}>{item.category}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
