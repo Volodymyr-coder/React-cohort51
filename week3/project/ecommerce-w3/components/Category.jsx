e3731f5e51d5cb55db46153b613c44d93a6f82b9
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2025-04-29 11:33:30 +0200

    add categoryproduct and css

---
+++ week3/project/ecommerce-w3/components/Category.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import CategoryProducts from './CategoryProduct';
+
+const Category = ({ category, onCategoryClick }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (item) => {
+    navigate(`/categories/category/${item}`);
+    onCategoryClick();
+  };
+
+  return (
+    <div>
+      {category.map((item) => {
+        return (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        );
+      })}
+      <Outlet />
+    </div>
+  );
+};
+
+export default Category;
---
+++ week3/project/ecommerce-w3/components/CategoryProduct.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { fetchAllData } from '../helpers/fetchData';
+import Loader from '../helpers/Loader';
+import css from './CategoryProduct.module.css';
+
+const CategoryProducts = () => {
+  const { category } = useParams();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const data = await fetchAllData(
+          `https://fakestoreapi.com/products/category/${category}`
+        );
+        setProducts(data);
+      } catch (error) {
+        console.error('Failed to fetch products:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProducts();
+  }, [category]);
+
+  const handleProductClick = (id) => {
+    navigate(`/categories/product/${id}`);
+  };
+
+  if (loading) return <Loader />;
+
+  return (
+    <ul className={css.gridContainer}>
+      {products.map((item) => (
+        <li
+          className={css.item}
+          key={item.id}
+          onClick={() => {
+            handleProductClick(item.id);
+          }}
+        >
+          <img className={css.img} src={item.image} alt={item.title} />
+          <p className={css.title}>{item.title}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CategoryProducts;
